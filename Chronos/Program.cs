e4e38e8fb15f1186161857f6fe736aeec5c0bfdd
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2026-04-27 14:04:59 +0200

    chore(global): List all the commands I'll have to do

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -10,8 +10,31 @@ string command = args[0];
 switch (command)
 {
     case "-i":
-    case "--init":
+    case "init":
         ps.initProject();
+        break;
+    case "-a":
+    case "add":
+        Console.WriteLine("Add command not implemented yet.");
+        break;
+    case "-c":
+    case "commit":
+        Console.WriteLine("Commit command not implemented yet.");
+        break;
+    case "-l":
+    case "log":
+        Console.WriteLine("Log command not implemented yet.");
+        break;
+    case "-s":
+    case "status":
+        Console.WriteLine("Status command not implemented yet.");
+        break;
+    case "checkout":
+        Console.WriteLine("Checkout command not implemented yet.");
+        break;
+    case "-h":
+    case "--help":
+        Console.WriteLine("Helper command not implemented yet.");
         break;
     default:
         Console.WriteLine($"Unknown command: {command}");
